018d2e91cc272329ee0afcfa118511e99863ab49
Mohamud Aynab <mfaynab88@gmail.com>
2025-04-03 14:44:58 -0700

    Selecting elements on the DOM practice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(document);
+const header = document.querySelector("h1");
+console.log(header);
+
+const value = document.querySelector(".value");
+console.log(value);
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+const area = document.querySelector(".area");
+console.log(area);
+
+const statDiv = document.querySelector(".stat div");
+console.log(statDiv);
+
+const hello = document.querySelector(".hello");
+console.log(hello);
